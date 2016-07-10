187a14ed7778590e43eb06daf0d88ab7d17759b3
Sean Madhavan <sean.madhavan@gmail.com>
2016-07-10 17:17:28 +0800

    fixes export

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var lib = require('./dist');
+var expressCheckout = require('./dist');
 var parallelPayment = require('./dist/parallel-payment');
 var referencePayment = require('./dist/reference-payment');
 
-exports.ParallelPayment = require('./dist/parallel-payment');
-exports.ReferencePayment = require('./dist/reference-payment');
-
-module.exports = lib;
+module.exports = {
+  default: expressCheckout,
+  ReferencePayment: referencePayment,
+  ParallelPayment: parallelPayment
+};

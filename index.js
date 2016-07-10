fa75e1678aa62b1026ef48aff433c688e492901c
Sean Madhavan <sean.madhavan@gmail.com>
2016-07-10 17:34:42 +0800

    export fix for multiple classes

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var expressCheckout = require('./dist');
 var parallelPayment = require('./dist/parallel-payment');
 var referencePayment = require('./dist/reference-payment');
 
-module.exports = {
-  default: expressCheckout,
-  ReferencePayment: referencePayment,
-  ParallelPayment: parallelPayment
-};
+module.exports = expressCheckout;
+module.exports.ReferencePayment = referencePayment.default;
+module.exports.ParallelPayment = parallelPayment.default;

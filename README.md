5a22486079a4f15683b4ec30044bff91b9d6b60f
Sean Madhavan <sean.madhavan@gmail.com>
2016-07-11 23:08:01 +0800

    merged classes

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # paypal-express-checkout
 Nodejs library for paypal express checkout (nvp)
+
+## Building
+``` sh
+gulp build
+```
+
+## Usage
+
+* initiating instance
+```js
+import Paypal from 'paypal-express-checkout';
+
+const config = {
+  USER, PWD, SIGNATURE, isSandbox
+};
+
+const paypal = new Paypal(config);
+
+```
--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var expressCheckout = require('./dist');
-var parallelPayment = require('./dist/parallel-payment');
-var referencePayment = require('./dist/reference-payment');
-
-module.exports = expressCheckout;
-module.exports.ReferencePayment = referencePayment.default;
-module.exports.ParallelPayment = parallelPayment.default;
+module.exports = require('./dist');
